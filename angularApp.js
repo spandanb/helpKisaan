1d1faf0f3adbdc0c07cfc5488f815065d8c09b4d
spandanb <spandan.777@gmail.com>
2015-01-01 18:01:11 -0500

    Display project list and allow user to submit projects

--- angularApp.js
+++ angularApp.js
@@ -1,6 +1,19 @@
 angular.module('kiva', [])
 .controller('MainCtrl', ['$scope', function($scope){
-    $scope.list = ['a','b','c'];
+    $scope.proj_list = [
+        {'name':"Buy Seeds", 'owner':'Jay Fernando', 'goal': 475, 'supporters':['Tom']},
+        {'name':"Buy Cows", 'owner':'Mai Toni', 'goal': 45, 'supporters':['Fred']},
+    ];
 
+    $scope.addProject = function(){
+        $scope.proj_list.push({
+            'name':$scope.name,
+            'owner':$scope.owner,
+            'goal':$scope.goal,
+        });
+        $scope.owner = null;
+        $scope.name = null;
+        $scope.goal = null;
+    }
 
 }]);
