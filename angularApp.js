5e7758e01618f170bd1bb70dced561366bca9973
spandanb <spandan.777@gmail.com>
2015-01-02 12:48:53 -0500

    Added service to persist data

--- angularApp.js
+++ angularApp.js
@@ -1,12 +1,22 @@
 angular.module('kiva', [])
-.controller('MainCtrl', ['$scope', function($scope){
-    $scope.proj_list = [
-        {'name':"Buy Seeds", 'owner':'Jay Fernando', 'goal': 475, 'supporters':['Tom']},
-        {'name':"Buy Cows", 'owner':'Mai Toni', 'goal': 45, 'supporters':['Fred']},
-    ];
+.factory('projects',[function(){
+    var p = {
+        projects:[]
+   };
+    return p;
+}])
+.controller('MainCtrl', [
+'$scope', 
+'projects',
+function($scope, projects){
+    $scope.projects = projects.projects;
+//    [
+//        {'name':"Buy Seeds", 'owner':'Jay Fernando', 'goal': 475, 'supporters':['Tom']},
+//        {'name':"Buy Cows", 'owner':'Mai Toni', 'goal': 45, 'supporters':['Fred']},
+//    ];
 
     $scope.addProject = function(){
-        $scope.proj_list.push({
+        $scope.projects.push({
             'name':$scope.name,
             'owner':$scope.owner,
             'goal':$scope.goal,
